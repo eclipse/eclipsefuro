13db8d44f7e38b3b7fdf297e851b3cf81019553a
veith <veithz@gmail.com>
2022-01-26 13:19:27 +0100

    feat: enum in env spec as int32 with Optionlist

--- furo/internal/cmd/genEsModule/genEsModule.go
+++ furo/internal/cmd/genEsModule/genEsModule.go
@@ -40,6 +40,33 @@ func Run(cmd *cobra.Command, args []string) {
 	clientspec.AddServicesToResolver(Servicelist.InstalledServicesByName)
 	allServices := clientspec.GetAllServices()
 
+	// check for enum fields in types and add the options
+	for k, t := range allTypes {
+		if t.Fields != nil {
+			// convert fields from yaml.Node to Field type
+			for pair := t.Fields.Oldest(); pair != nil; pair = pair.Next() {
+				field := pair.Value.(*clientspec.Field)
+
+				if allTypes[field.Type] != nil && allTypes[field.Type].Values != nil {
+					field.Meta.Options.Flags = append(field.Meta.Options.Flags, "enum")
+
+					// loop all enum values to build the options
+					for enum := allTypes[field.Type].Values.Oldest(); enum != nil; enum = enum.Next() {
+						option := map[string]interface{}{}
+						option["display_name"] = strings.ToLower("enum." + field.Type + "." + enum.Key.(string) + ".label")
+						option["id"] = enum.Value.(*uint32)
+						option["@type"] = "type.googleapis.com/furo.Optionitem"
+						option["selected"] = false
+						field.Meta.Options.List = append(field.Meta.Options.List, &option)
+					}
+
+					fmt.Println(k, allTypes[field.Type].Values)
+				}
+			}
+		}
+
+	}
+
 	td, _ := json.Marshal(allTypes)
 
 	// only escape " \n and \t and not just \ because we have strings like \u003c which is a <
